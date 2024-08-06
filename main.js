142526b60a41f3117749d063dcbb60950685832f
prutmongkol <150164864+PrutMongkol@users.noreply.github.com>
2024-08-06 17:31:41 +0700

    add command line argument

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+import { argv } from "process";
+
+function main() {
+    if (argv.length < 3) {
+        console.log("Need a url argument.\nUsage: node main.js <baseURL>");
+        return;        
+    }
+    if (argv.length > 3) {
+        console.log("Too many arguments\nUsage: node main.js <baseURL>");
+        return;
+    }
+
+    const baseURL = argv[2];
+    console.log(`Crawler is starting at the following base URL: ${baseURL}`);    
+}
+
+main()
