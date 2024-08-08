6e4dcb02a629b8cd4e29321e3acee84fa408fbb0
prutmongkol <150164864+PrutMongkol@users.noreply.github.com>
2024-08-08 19:48:43 +0700

    fix crawlPage logic

--- crawl.js
+++ crawl.js
@@ -30,45 +30,52 @@ async function fetchPage(url) {
     let res;
     try {
         res = await fetch(url);
-        if (res.status >= 400) {
-            console.log(`HTTP error: ${res.status} ${res.statusText}`);
-            return;
-        }
-        const contentType = res.headers.get("Content-Type");
-        if (!contentType || !contentType.includes("text/html")) {
-            console.log(`Invalid content type: ${contentType}`);
-            return;
-        }
     } catch (error) {
         console.log(error.message);
+    }
+
+    if (res.status >= 400) {
+        console.log(`HTTP error: ${res.status} ${res.statusText}`);
+        return;
+    }
+    const contentType = res.headers.get("Content-Type");
+    if (!contentType || !contentType.includes("text/html")) {
+        console.log(`Invalid content type: ${contentType}`);
+        return;
     }
 
     return await res.text();
 }
 
 async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
-    console.log(`Crawling ${currentURL}`);
-
+    // bail if this is an offsite url
     const baseURLObject = new URL(baseURL);
     const currentURLObject = new URL(currentURL);
-
     if (baseURLObject.origin !== currentURLObject.origin) {
-        console.log("\tUnmatched base url. Terminated.");
         return pages;
     }
 
     const normalizedCurrentURL = normalizeURL(currentURL);
 
-    pages[normalizedCurrentURL] = (pages[normalizedCurrentURL] || 0) + 1;
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++;
+        return pages;
+    }
 
-    const html = await fetchPage(currentURL);
+    pages[normalizedCurrentURL] = 1;
+
+    console.log(`crawling ${currentURL}`);
+    let html = "";
+    try {
+        html = await fetchPage(currentURL);
+    } catch (error) {
+        console.log(`${error.message}`);
+        return pages;
+    }
+
     const urls = getURLsFromHTML(html, baseURL);
-
     for (const url of urls) {
-        if (Object.hasOwn(pages, normalizeURL(url))) {
-            continue;
-        }
-        await crawlPage(baseURL, url, pages);
+        pages = await crawlPage(baseURL, url, pages);
     }
 
     return pages;
